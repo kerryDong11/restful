23d6f5891b08735b242d59c1e4b86b62112ff591
kerryDong11 <55862830+kerryDong11@users.noreply.github.com>
2021-03-03 20:51:55 +0800

    update user.py to_dict()

--- router/User.py
+++ router/User.py
@@ -8,26 +8,35 @@ from models.db import db
 
 class User(Resource):
     def get(self, user_id):
+        print (user_id)
         user = UserModel.query.filter_by(username=user_id).first()
         if user:
-            return user.json()
+
+            return user.to_dict()
         return {'message': 'User not found'}, 404
+
+    # def get(self,user_id):
+    #     return ''
 
 
 
 class UserList(Resource):
     def get(self):
 
-        return {'users': [user.json() for user in UserModel.query.all()]}
+        return {'users': [user.to_dict() for user in UserModel.query.all()]}
     
     def post(self):
            
         # 查询该国家是否存在于数据库中
+<<<<<<< Updated upstream
             name = request.json['username']
             u_id =  request.json['u_id']
             #udata = request.get_json()
            
             user = UserModel(username = name,u_id = u_id)
+=======
+            user = UserModel().from_dict(request.json)
+>>>>>>> Stashed changes
             print(user)
             db.session.add(user)
             db.session.commit()
